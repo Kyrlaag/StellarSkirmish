ff307a58253c98d4a7ec7d4040aceac36bbbd600
Aykut ÖKSÜZ <aykutoksuz.124@gmail.com>
2024-06-05 00:58:16 +0300

    Uploading project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+
+    [SerializeField] int scorePerHit = 20;
+    [SerializeField] int hitPoints = 10;
+
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        hitPoints--;
+    }
+
+    void KillEnemy()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        Destroy(gameObject);
+    }
+}
